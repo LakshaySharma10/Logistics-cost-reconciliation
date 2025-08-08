5aa0189fe5be3abfab1b1eb652488f2d5f164306
Lakshay Sharma <lakshay7004@gmail.com>
2025-08-08 13:25:26 +0530

    deployment fix backend

--- frnd/frnd/settings.py
+++ frnd/frnd/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-v2ofkx%upi*bn*y$d6*v2o=v5@8dis2%&nv!@*!hk(7(uxrgg9'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -119,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
